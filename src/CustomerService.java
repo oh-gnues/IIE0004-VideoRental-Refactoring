d2bdf6bef670f047f196e9e6542a4ce1b2c3888d
KimSeogwon <tjrnjs123@gmail.com>
2024-12-07 22:56:16 +0900

    CustomerService 로직 구성 및 고객 검색 메서드 (findCustomer)추가

--- src/CustomerService.java
+++ src/CustomerService.java
@@ -1,4 +1,36 @@
 package src;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class CustomerService {
+    private List<Customer> customers;
+
+    public CustomerService() {
+        customers = new ArrayList<Customer>();
+    }
+
+    public List<String> listCustomers() {
+        List<String> customerInfo = new ArrayList<>();
+        for (Customer customer : customers) {
+            customerInfo.add("Name: " + customer.getName() + ", Rentals: " + customer.getRentals().size());
+        }
+        return customerInfo;
+    }
+
+    public void registerCustomer(String name) {
+        customers.add(new Customer(name));
+    }
+
+    public Customer findCustomer(String name) {
+        return customers.stream()
+                .filter(customer -> customer.getName().equals(name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public void initDefaultCustomers() {
+        customers.add(new Customer("James"));
+        customers.add(new Customer("Brown"));
+    }
 }
