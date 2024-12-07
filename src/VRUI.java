f7f9b53c530112b716f923dbfcbab616a984d4f4
KimSeogwon <tjrnjs123@gmail.com>
2024-12-08 00:05:03 +0900

    RentalService 함수 순서 변경 및 불필요 로컬 변수 제거

--- src/VRUI.java
+++ src/VRUI.java
@@ -1,8 +1,5 @@
 package src;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 import java.util.Scanner;
 
 public class VRUI {
@@ -26,53 +23,85 @@ public class VRUI {
 				case 4: ui.registerVideo() ; break ;
 				case 5: ui.rentVideo() ; break ;
 				case 6: ui.returnVideo() ; break ;
-				case 7: ui.getCustomerReport() ; break; 
+				case 7: ui.getCustomerReport() ; break ;
 				case 8: ui.clearRentals() ; break ;
 				case -1: ui.init() ; break ;
 				default: break ;
 			}
 		}
-		System.out.println("Bye");
+		System.out.println("Bye") ;
 	}
 
-	public void clearRentals() {
+	public int showCommand() {
+		System.out.println("\nSelect a command !") ;
+		System.out.println("\t 0. Quit") ;
+		System.out.println("\t 1. List customers") ;
+		System.out.println("\t 2. List videos") ;
+		System.out.println("\t 3. Register customer") ;
+		System.out.println("\t 4. Register video") ;
+		System.out.println("\t 5. Rent video") ;
+		System.out.println("\t 6. Return video") ;
+		System.out.println("\t 7. Show customer report") ;
+		System.out.println("\t 8. Show customer and clear rentals") ;
+
+		return scanner.nextInt() ;
+	}
+
+	public void listCustomers() {
+		System.out.println("List of customers") ;
+		for ( String customerDetail: customerService.listCustomers() ) {
+			System.out.println(customerDetail) ;
+		}
+		System.out.println("End of list") ;
+	}
+
+	public void listVideos() {
+		System.out.println("List of videos") ;
+
+		for ( String videoInfo: videoService.listVideos() ) {
+			System.out.println(videoInfo) ;
+		}
+		System.out.println("End of list") ;
+	}
+
+	public void registerCustomer() {
+		System.out.println("Enter customer name: ") ;
+		String name = scanner.next() ;
+
+		customerService.registerCustomer(name) ;
+	}
+
+	public void registerVideo() {
+		System.out.println("Enter video title to register: ") ;
+		String title = scanner.next() ;
+
+		System.out.println("Enter video type( 1 for VHD, 2 for CD, 3 for DVD ):") ;
+		int videoType = scanner.nextInt() ;
+
+		System.out.println("Enter price code( 1 for Regular, 2 for New Release ):") ;
+		int priceCode = scanner.nextInt() ;
+
+		videoService.registerVideo(title, videoType, priceCode) ;
+	}
+
+	public void rentVideo() {
 		System.out.println("Enter customer name: ") ;
 		String customerName = scanner.next() ;
 
-		rentalService.clearRentals(customerName) ;
+		System.out.println("Enter video title to rent: ") ;
+		String videoTitle = scanner.next() ;
+
+		rentalService.rentVideo(customerName, videoTitle);
 	}
 
 	public void returnVideo() {
 		System.out.println("Enter customer name: ") ;
 		String customerName = scanner.next() ;
-		
+
 		System.out.println("Enter video title to return: ") ;
 		String videoTitle = scanner.next() ;
 
 		rentalService.returnVideo(customerName, videoTitle) ;
-	}
-
-	private void init() {
-		customerService.initDefaultCustomers();
-		videoService.initDefaultVideos();
-		rentalService.initDefaultRentals();
-	}
-
-	public void listVideos() {
-		System.out.println("List of videos");
-
-		for ( String videoInfo: videoService.listVideos() ) {
-			System.out.println(videoInfo) ;
-		}
-		System.out.println("End of list");
-	}
-
-	public void listCustomers() {
-		System.out.println("List of customers");
-		for ( String customerDetail: customerService.listCustomers() ) {
-			System.out.println(customerDetail);
-		}
-		System.out.println("End of list");
 	}
 
 	public void getCustomerReport() {
@@ -82,50 +111,16 @@ public class VRUI {
 		System.out.println(rentalService.getCustomerReport(customerName));
 	}
 
-	public void rentVideo() {
+	public void clearRentals() {
 		System.out.println("Enter customer name: ") ;
 		String customerName = scanner.next() ;
-		
-		System.out.println("Enter video title to rent: ") ;
-		String videoTitle = scanner.next() ;
 
-		rentalService.rentVideo(customerName, videoTitle);
+		rentalService.clearRentals(customerName) ;
 	}
 
-	public void registerCustomer() {
-		System.out.println("Enter customer name: ") ;
-		String name = scanner.next();
-		customerService.registerCustomer(name) ;
-	}
-
-	public void registerVideo() {
-		System.out.println("Enter video title to register: ") ;
-		String title = scanner.next() ;
-
-		System.out.println("Enter video type( 1 for VHD, 2 for CD, 3 for DVD ):") ;
-		int videoType = scanner.nextInt();
-
-		System.out.println("Enter price code( 1 for Regular, 2 for New Release ):") ;
-		int priceCode = scanner.nextInt();
-
-		videoService.registerVideo(title, videoType, priceCode) ;
-	}
-
-	public int showCommand() {
-		System.out.println("\nSelect a command !");
-		System.out.println("\t 0. Quit");
-		System.out.println("\t 1. List customers");
-		System.out.println("\t 2. List videos");
-		System.out.println("\t 3. Register customer");
-		System.out.println("\t 4. Register video");
-		System.out.println("\t 5. Rent video");
-		System.out.println("\t 6. Return video");
-		System.out.println("\t 7. Show customer report");
-		System.out.println("\t 8. Show customer and clear rentals");
-		
-		int command = scanner.nextInt() ;
-		
-		return command ;
-		
+	private void init() {
+		customerService.initDefaultCustomers() ;
+		videoService.initDefaultVideos() ;
+		rentalService.initDefaultRentals() ;
 	}
 }
